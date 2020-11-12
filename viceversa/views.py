49d82c55145d92d24270ddf7d4ed23dd16061c1c
Andrey <skamsk@list.ru>
2020-11-12 11:38:34 +0500

    еще 1 коммит

--- viceversa/views.py
+++ viceversa/views.py
@@ -9,5 +9,22 @@ def home(request):
 
 def reverse(request):
     text = request.GET['usertext']
+    words = 0
+    pos = 'out'
+    for letter in text:
+        # Если очередной символ не пробел, а флаг в значении "вне слова",
+        # то значит начинается новое слово.
+        if letter != ' ' and pos == 'out':
+            # Поэтому надо увеличить счетчик слов на 1,
+            words += 1
+            # а флаг поменять на значение "внутри слова".
+            pos = 'in'
+        # Если очередной символ пробел,
+        elif letter == ' ':
+            # то следует установить флаг в значение "вне слова".
+            pos = 'out'
+    kol = words
+    print(kol)
     get_text = text[::-1]
-    return render(request, 'reverse.html', {'user_text':text, 'reverse_text':get_text})
+
+    return render(request, 'reverse.html', {'user_text': text, 'reverse_text': get_text, 'kol': kol})
