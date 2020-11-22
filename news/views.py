5c5c3d089fbc85295938dc19ec6c230238f68532
Andrey <skamsk@list.ru>
2020-11-22 21:14:13 +0500

    add inclusion_tags

--- news/views.py
+++ news/views.py
@@ -6,11 +6,9 @@ from .models import News, Category
 # Create your views here.
 def index(request):
     news = News.objects.order_by('-created_at')
-    categories = Category.objects.all()
     context = {
         'news': news,
         'title': 'Список новостей',
-        'categories': categories,
     }
     return render(request, 'news/index.html', context)
 
@@ -20,11 +18,9 @@ def test(request):
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id = category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
     context = {
         'news': news,
         'category': category,
-        'categories': categories,
     }
     return render(request, 'news/category.html', context)
