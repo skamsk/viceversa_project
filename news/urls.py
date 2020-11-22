7ddaa586bb5ab53eda1df5cab5bac61499332557
Andrey <skamsk@list.ru>
2020-11-22 22:05:10 +0500

    add view_news.html

--- news/urls.py
+++ news/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', index, name='home'),
     path('about/', test, name='about'),
     path('category/<int:category_id>/', get_category, name='category'),
+    path('news/<int:news_id>/', view_news, name='view_news'),
 ]
