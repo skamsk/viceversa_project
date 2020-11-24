815c59e2288e38d4e05edd5666697b5c496519be
Andrey <skamsk@list.ru>
2020-11-24 22:27:50 +0500

    rework full project

--- viceversa/settings.py
+++ viceversa/settings.py
@@ -119,5 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'news/static'),
+        os.path.join(BASE_DIR, 'viceversa/static')
+
+]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
