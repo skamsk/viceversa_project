4957191e7080d57c929945a1e338b61adca4ef39
Andrey <skamsk@list.ru>
2020-11-29 21:25:40 +0500

    add class DetailView

--- news/views.py
+++ news/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import News, Category
 from .forms import NewsForm
 
@@ -20,13 +20,6 @@ class HomeNews(ListView):
 
 
 
-# def index(request):
-#     news = News.objects.order_by('-created_at')
-#     context = {
-#         'news': news,
-#         'title': 'Список новостей',
-#     }
-#     return render(request, 'news/index.html', context)
 
 def test(request):
     return HttpResponse("<H1>Тестовая страница</H1>")
@@ -46,16 +39,11 @@ class NewsByCategory(ListView):
     def get_queryset(self):
         return News.objects.filter(category_id = self.kwargs['category_id'], is_published=True)
 
+class ViewNews(DetailView):
+    model = News
+    #pk_url_kwarg = 'news_id'
+    context_object_name = 'news_item'
 
-
-def get_category(request, category_id):
-    news = News.objects.filter(category_id = category_id)
-    category = Category.objects.get(pk=category_id)
-    context = {
-        'news': news,
-        'category': category,
-    }
-    return render(request, 'news/category.html', context)
 
 
 def view_news(request, news_id):
@@ -74,3 +62,21 @@ def add_news(request):
     else:
         form = NewsForm()
     return render(request, 'news/add_news.html', {'form': form})
+
+
+# def index(request):
+#     news = News.objects.order_by('-created_at')
+#     context = {
+#         'news': news,
+#         'title': 'Список новостей',
+#     }
+#     return render(request, 'news/index.html', context)
+
+# def get_category(request, category_id):
+#     news = News.objects.filter(category_id = category_id)
+#     category = Category.objects.get(pk=category_id)
+#     context = {
+#         'news': news,
+#         'category': category,
+#     }
+#     return render(request, 'news/category.html', context)
