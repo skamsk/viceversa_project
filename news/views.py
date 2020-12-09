4803d76beff466ae8fc0de2a4baba948e96a0d1a
Andrey <skamsk@list.ru>
2020-12-09 21:28:00 +0500

    add authorization  and logout

--- news/views.py
+++ news/views.py
@@ -3,29 +3,41 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 from .models import News, Category
-from .forms import NewsForm
+from .forms import NewsForm, UserRegisterForm, UserLoginForm
 from .utils import MyMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout
 from django.contrib import messages
 
 def register(request):
         if request.method == 'POST':
-            form = UserCreationForm(request.POST)
+            form = UserRegisterForm(request.POST)
             if form.is_valid():
-                form.save()
+                user = form.save()
+                login(request, user)
                 messages.success(request, 'Вы успешно зарегистрировались')
-                return redirect('login')
+                return redirect('home')
             else:
                 messages.error(request, 'Ошибка регистрации')
         else:
-            form = UserCreationForm()
+            form = UserRegisterForm()
         return render(request, 'news/register.html', {"form": form})
 
 
-def login(request):
-    return render(request, 'news/login.html')
+def user_login(request):
+    if request.method == "POST":
+        form = UserLoginForm(data = request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserLoginForm
+    return render(request, 'news/login.html',  {"form": form})
 
+def user_logout(request):
+    logout(request)
+    return redirect('login')
 
 class HomeNews(MyMixin, ListView):
     model = News
