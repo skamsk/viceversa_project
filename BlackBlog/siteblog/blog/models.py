a37dd8efc0c27efb4f74019de26b5f6d23319071
Andrey <skamsk@list.ru>
2020-12-16 21:01:03 +0500

    add header and footer menu

--- BlackBlog/siteblog/blog/models.py
+++ BlackBlog/siteblog/blog/models.py
@@ -1,4 +1,8 @@
+
+
 from django.db import models
+from django.urls import reverse
+
 
 class Category(models.Model):
     title = models.CharField(max_length=255)
@@ -6,6 +10,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={"slug": self.slug})
 
     class Meta:
         verbose_name = 'Категория(ю)'
