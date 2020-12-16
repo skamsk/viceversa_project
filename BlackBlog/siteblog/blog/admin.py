cf61ec43869ef1f381db0f306fcc9ba66c1cdc2d
Andrey <skamsk@list.ru>
2020-12-16 19:15:58 +0500

    add new properties to admin panel

--- BlackBlog/siteblog/blog/admin.py
+++ BlackBlog/siteblog/blog/admin.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib import admin
 from ckeditor.widgets import CKEditorWidget
 from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django.utils.safestring import mark_safe
+
 from .models import *
 
 
@@ -14,6 +16,22 @@ class PostAdminForm(forms.ModelForm):
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ("title",)}
     form = PostAdminForm
+    save_as = True
+    save_on_top = True
+    list_display = ('id', 'title', 'slug', 'category', 'created_at', 'get_photo')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'id', 'slug')
+    list_filter = ('category',)
+    readonly_fields = ('views', 'created_at', 'get_photo')
+    fields = ('title', 'slug', 'category', 'tags', 'content', 'photo', 'get_photo', 'views', 'created_at')
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="50">')
+        return '--'
+
+    get_photo.short_description = 'Фото'
+
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ("title",)}
--- BlackBlog/siteblog/blog/models.py
+++ BlackBlog/siteblog/blog/models.py
@@ -8,6 +8,8 @@ class Category(models.Model):
         return self.title
 
     class Meta:
+        verbose_name = 'Категория(ю)'
+        verbose_name_plural = 'Категории'
         ordering = ['title']
 
 
@@ -19,22 +21,26 @@ class Tag(models.Model):
         return self.title
 
     class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
         ordering = ['title']
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=50, verbose_name='Заголовок')
     slug = models.SlugField(max_length=50, verbose_name='Url', unique=True)
-    author = models.CharField(max_length=100)
+    author = models.CharField(max_length=100, verbose_name= 'Автор')
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Опубликовано')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     views = models.IntegerField(default=0, verbose_name='Количество просмотров')
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts', verbose_name='Категория')
     tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
 
     def __str__(self):
         return self.title
 
     class Meta:
+        verbose_name = 'Статья(ю)'
+        verbose_name_plural = 'Статьи'
         ordering = ['-created_at']
