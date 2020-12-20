f65d1b14f205f4336e0ad5e39f74d72e8bb19a4d
Andrey <skamsk@list.ru>
2020-12-20 21:53:19 +0500

    add tags block

--- BlackBlog/siteblog/blog/templatetags/sidebar.py
+++ BlackBlog/siteblog/blog/templatetags/sidebar.py
@@ -1,5 +1,5 @@
 from django import template
-from blog.models import Post
+from blog.models import Post, Tag
 
 register = template.Library()
 
@@ -7,3 +7,10 @@ register = template.Library()
 def get_popular(cnt=3):
     post = Post.objects.order_by('-views')[:cnt]
     return {"posts": post}
+
+
+@register.inclusion_tag('blog/tags_tpl.html')
+def get_tags(cnt=3):
+    tags = Tag.objects.all()
+    return {"tags": tags}
+
